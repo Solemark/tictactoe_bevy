7abec51b9119ce4fdca5a0c229f82635cbe2e24b
Solemarc <larco9mason@gmail.com>
2025-02-10 21:00:30 +1000

    complete logic for game win

--- src/app.rs
+++ src/app.rs
@@ -10,6 +10,7 @@ use bevy::{
     },
     core_pipeline::core_2d::Camera2d,
     ecs::{
+        entity::Entity,
         query::{Changed, With},
         system::{Commands, Query, Res, ResMut},
     },
@@ -29,7 +30,7 @@ pub fn start() {
         .add_plugins(DefaultPlugins)
         .insert_resource(WinitSettings::desktop_app())
         .add_systems(Startup, setup)
-        .add_systems(Update, (button_system, status_system))
+        .add_systems(Update, (button_system, status_system, win_system))
         .run();
 }
 
@@ -71,7 +72,7 @@ pub fn button_system(
         let mut text = text_query.get_mut(children[0]).unwrap();
         match *interaction {
             Interaction::Pressed => {
-                if text.0 == "_" {
+                if !status.0.contains("win") && text.0 == "_" {
                     if turn.0 % 2 == 0 {
                         text.0 = "X".to_string();
                         status.0 = "O's turn".to_string();
@@ -83,7 +84,7 @@ pub fn button_system(
                 }
             }
             Interaction::Hovered => {
-                if text.0 == "_" {
+                if !status.0.contains("win") && text.0 == "_" {
                     border_color.0 = WHITE.into();
                 } else {
                     border_color.0 = RED.into();
@@ -97,4 +98,45 @@ pub fn button_system(
 pub fn status_system(status: Res<Status>, mut text_query: Query<&mut Text, With<StatusUI>>) {
     let mut text = text_query.single_mut();
     text.0 = status.0.to_string();
+}
+
+pub fn win_system(
+    mut status: ResMut<Status>,
+    query: Query<(Entity, &Children), With<Button>>,
+    text_query: Query<&mut Text>,
+) {
+    let mut board = Vec::new();
+    for (e, c) in &query {
+        let text = text_query.get(c[0]).unwrap();
+        board.push((e.to_string(), text.0.clone()));
+    }
+    board.sort();
+    if validate_tiles(&board[0].1, &board[1].1, &board[2].1) {
+        status.0 = format!("{} wins", board[0].1);
+    }
+    if validate_tiles(&board[3].1, &board[4].1, &board[5].1) {
+        status.0 = format!("{} wins", board[3].1);
+    }
+    if validate_tiles(&board[6].1, &board[7].1, &board[8].1) {
+        status.0 = format!("{} wins", board[6].1);
+    }
+    if validate_tiles(&board[0].1, &board[3].1, &board[6].1) {
+        status.0 = format!("{} wins", board[0].1);
+    }
+    if validate_tiles(&board[1].1, &board[4].1, &board[7].1) {
+        status.0 = format!("{} wins", board[1].1);
+    }
+    if validate_tiles(&board[2].1, &board[5].1, &board[8].1) {
+        status.0 = format!("{} wins", board[2].1);
+    }
+    if validate_tiles(&board[0].1, &board[4].1, &board[8].1) {
+        status.0 = format!("{} wins", board[0].1);
+    }
+    if validate_tiles(&board[2].1, &board[4].1, &board[6].1) {
+        status.0 = format!("{} wins", board[2].1);
+    }
+}
+
+fn validate_tiles(x: &str, y: &str, z: &str) -> bool {
+    x == y && x == z && x != "_"
 }
